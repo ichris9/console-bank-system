850fd5863dc95e76143a9a233e6240035abb271d
Christian Hugo <Christian.h2k6@gmail.com>
2026-02-02 22:07:53 -0300

    classe view para a classe service:contaCorrente

--- src/main/java/view/ViewContaCorrente.java
+++ src/main/java/view/ViewContaCorrente.java
@@ -1,4 +1,21 @@
 package view;
-public class ViewContaCorrente {
+
+import model.Conta;
+import model.Usuario;
+import service.ContaCorrente;
+
+    public class ViewContaCorrente {
     
+        public String viewToString(Usuario user, ContaCorrente cc){
+            return "\n================="
+                    +"nome: " + user.getNome()
+                    +"cpf: "  + user.getCpf()
+                    +"Saldo: "+ cc.getSaldoDaConta();
+        }
+        public void viewCC(Usuario user, ContaCorrente cc){
+            System.out.println("\n=================="
+                    + "\nnome: " + user.getNome()
+                    + "\ncpf: " + user.getCpf()
+                    + "\nsaldo: " + cc.getSaldoDaConta());
+        }
 }
