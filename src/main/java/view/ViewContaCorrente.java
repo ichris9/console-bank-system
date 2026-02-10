4c550ea1ec03b87a63767a95ddfd3d2d1d9acba9
Christian Hugo <Christian.h2k6@gmail.com>
2026-02-09 23:00:51 -0300

    método de visualização de depósito

--- src/main/java/view/ViewContaCorrente.java
+++ src/main/java/view/ViewContaCorrente.java
@@ -1,21 +1,25 @@
 package view;
 
 import model.Conta;
-import model.Usuario;
+import model.Cliente;
 import service.ContaCorrente;
 
     public class ViewContaCorrente {
     
-        public String viewToString(Usuario user, ContaCorrente cc){
+        public String viewToString(Cliente user, ContaCorrente cc){
             return "\n================="
                     +"nome: " + user.getNome()
                     +"cpf: "  + user.getCpf()
                     +"Saldo: "+ cc.getSaldoDaConta();
         }
-        public void viewCC(Usuario user, ContaCorrente cc){
+        public void viewCC(Cliente user, ContaCorrente cc){
             System.out.println("\n=================="
                     + "\nnome: " + user.getNome()
                     + "\ncpf: " + user.getCpf()
                     + "\nsaldo: " + cc.getSaldoDaConta());
+        }
+        
+        public void depositadoComSucesso(double valor, Conta conta){
+            System.out.print("O valor de " + valor + " foi depositado na conta de: " + conta.cliente.getNome());
         }
 }
