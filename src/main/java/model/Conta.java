795a0de40d8390ff6db74663e907a3ec60ac76b2
Christian Hugo <Christian.h2k6@gmail.com>
2026-02-09 23:09:07 -0300

    método abstrato de depósito e método 'alterarSaldoDaConta' para manipulaçoes do saldo

--- src/main/java/model/Conta.java
+++ src/main/java/model/Conta.java
@@ -1,9 +1,11 @@
 package model;
 public abstract class Conta {
     protected double saldoDaConta;
+    public Cliente cliente;
     
-    public Conta(Usuario user){
-        this.saldoDaConta = 0;
+    public Conta(Cliente user){
+       this.saldoDaConta = 0;
+       this.cliente = user;
     }
     
     //getter
@@ -14,6 +16,11 @@ public abstract class Conta {
     //métodos abstrato
     public abstract void fazerPix(double valor, Conta conta);
     
+    public abstract void depositarNaConta(double valor, Conta conta);
+    
+    public void alterarSaldo(double valor){
+        this.saldoDaConta += valor;
+    }    
     
         
     
