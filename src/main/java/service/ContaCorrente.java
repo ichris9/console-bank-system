f3e3790e39fd0cf28afce9bfce26fec24501a675
Christian Hugo <Christian.h2k6@gmail.com>
2026-02-09 23:04:20 -0300

    método 'depósitarNaConta' para injeção de cash inicial

--- src/main/java/service/ContaCorrente.java
+++ src/main/java/service/ContaCorrente.java
@@ -1,11 +1,12 @@
 package service;
 
 import model.Conta;
-import model.Usuario;
+import model.Cliente;
+import view.ViewContaCorrente;
 
 public class ContaCorrente extends Conta {
     
-    public ContaCorrente(Usuario user){
+    public ContaCorrente(Cliente user){
         super(user);
     }
     
@@ -14,7 +15,15 @@ public class ContaCorrente extends Conta {
         if(saldoDaConta < valor || conta.equals(""))throw new RuntimeException("Saldo insuficiente ou usuário não existe");
             this.saldoDaConta -= valor;
             super.saldoDaConta += valor;
-            
+    }
+    @Override
+    public void depositarNaConta(double valor, Conta conta){
+       if(valor < 0){
+           throw new IllegalArgumentException("Valor invalido! insira um valor válido acima de 0!");
+       }conta.alterarSaldo(valor);
         
+        //view
+        ViewContaCorrente view = new ViewContaCorrente();
+        view.depositadoComSucesso(valor, conta);
     }
 }
